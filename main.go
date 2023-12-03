bb2e0fef52b87facd05012a8286590d506164eab
James Pritchard <james.edward.pritchard@gmail.com>
2023-12-03 13:44:11 -0500

    third commit

--- main.go
+++ main.go
@@ -10,24 +10,25 @@ func main() {
 	defer func() {
 		fmt.Println("Time elapsed:", time.Since(start))
 	}()
-	examplechannel := make(chan bool)
-	evilNinjas := []string{"Hattori", "Yoshi", "Kuma", "Momochi"}
-	for _, ninja := range evilNinjas {
-		// go keyword is used to create a goroutine
-		// the attack function will be called in a goroutine
-		// on its own main func will finish before the goroutines are done
-		go attack(ninja, examplechannel)
 
-		// we can use the <- operator to receive a value from a channel
-		// <- is to read from a channel i.e. pulling it out of the channel
-		fmt.Println(<-examplechannel)
-	}
-	time.Sleep(time.Second)
-}
+	// THIS IS A CHANNEL
+	// channel := make(chan string)
+	//cannot just use channel <- "First Message" without a go routine
+	// this will create a deadlock as the channel cant hold information is just passes it through
+	// channel <- "First Message"
+	// go func() {
+	// 	channel <- "First Message"
+	// }()
 
-func attack(target string, attacked chan bool) {
-	fmt.Println("Attacking", target)
-	//when we want to add a value to a channel we use the <- operator pointing to the channel
-	// <- is to write to a channel i.e. pushing it into the channel
-	attacked <- true
+	// THIS IS A BUFFERED CHANNEL
+	// capacity is 1
+	// channel := make(chan string, 1)
+	// channel <- "First Message"
+
+	channel := make(chan string, 2)
+	channel <- "First Message"
+	channel <- "Second Message"
+
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
 }
