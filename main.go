c6d43af90394fbd29921f98a73ba7a81aebd5025
James Pritchard <james.edward.pritchard@gmail.com>
2023-12-03 14:04:56 -0500

    fifth commit

--- main.go
+++ main.go
@@ -11,21 +11,21 @@ func main() {
 	defer func() {
 		fmt.Println("Time elapsed:", time.Since(start))
 	}()
-	//iteratate through the channel
-	//this example is a ninja throwing stars at a target
-	numStarsToThrow := 500
-	channel := make(chan string, numStarsToThrow)
-	go throwNinjaStars(channel, numStarsToThrow)
-	for i := 0; i < numStarsToThrow; i++ {
-		fmt.Println(<-channel)
+	channel := make(chan string)
+	go throwNinjaStars(channel)
+	for message := range channel {
+		fmt.Println(message)
 	}
 
 }
 
-func throwNinjaStars(channel chan string, numStarsToThrow int) {
+func throwNinjaStars(channel chan string) {
+	numStarsToThrow := 20
 	for i := 0; i < numStarsToThrow; i++ {
 		//generate a random score between 1 and 10
 		score := 1 + rand.Intn(10)
 		channel <- fmt.Sprintf("You scored %d points!", score)
 	}
+	//this allows the for loop in main() to exit
+	close(channel)
 }
