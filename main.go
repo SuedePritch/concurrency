0bd29fc9be989b3c0b8e87a04d4fa071a6924b27
James Pritchard <james.edward.pritchard@gmail.com>
2023-12-03 13:19:58 -0500

    second commit

--- main.go
+++ main.go
@@ -10,16 +10,24 @@ func main() {
 	defer func() {
 		fmt.Println("Time elapsed:", time.Since(start))
 	}()
+	examplechannel := make(chan bool)
 	evilNinjas := []string{"Hattori", "Yoshi", "Kuma", "Momochi"}
 	for _, ninja := range evilNinjas {
 		// go keyword is used to create a goroutine
 		// the attack function will be called in a goroutine
-		// on its own it will fininsh before the goroutines are done
-		go attack(ninja)
+		// on its own main func will finish before the goroutines are done
+		go attack(ninja, examplechannel)
+
+		// we can use the <- operator to receive a value from a channel
+		// <- is to read from a channel i.e. pulling it out of the channel
+		fmt.Println(<-examplechannel)
 	}
 	time.Sleep(time.Second)
 }
 
-func attack(target string) {
+func attack(target string, attacked chan bool) {
 	fmt.Println("Attacking", target)
+	//when we want to add a value to a channel we use the <- operator pointing to the channel
+	// <- is to write to a channel i.e. pushing it into the channel
+	attacked <- true
 }
