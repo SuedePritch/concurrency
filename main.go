ea2f8e6292680fa7324c95562d7460a1519184ee
James Pritchard <james.edward.pritchard@gmail.com>
2023-12-04 22:48:11 -0500

    waitgroups

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -10,43 +11,25 @@ func main() {
 	defer func() {
 		fmt.Println("Time elapsed:", time.Since(start))
 	}()
-	//channel select statement
-	//the select statement lets a goroutine wait on multiple communication operations.
-	//A select blocks until one of its cases can run, then it executes that case.
-	//It chooses one at random if multiple are ready.
-	//The default case in a select is run if no other case is ready.
-	ninja1, ninja2 := make(chan string), make(chan string)
-	go captainElect(ninja1, "Ninja1")
-	go captainElect(ninja2, "Ninja2")
+	var evilNinjas = []string{"James", "Jenn", "John", "Jim"}
+	var attackers = []string{"Attacker1", "Attacker2", "Attacker3"}
+	var wg sync.WaitGroup
 
-	select {
-	case message := <-ninja1:
-		fmt.Println(message)
-	case message := <-ninja2:
-		fmt.Println(message)
-	default:
-		fmt.Println("No one is the captain")
+	for _, attacker := range attackers {
+		wg.Add(1)
+		go func(attacker string) {
+			defer wg.Done()
+			for _, evilNinja := range evilNinjas {
+				wg.Add(1)
+				go attack(attacker, evilNinja, &wg)
+			}
+		}(attacker)
 	}
-	roughlyFair()
+
+	wg.Wait()
 }
 
-func captainElect(ninja chan string, message string) {
-	ninja <- message
-}
-func roughlyFair() {
-	ninja1 := make(chan interface{})
-	close(ninja1)
-	ninja2 := make(chan interface{})
-	close(ninja2)
-
-	var ninja1Count, ninja2Count int
-	for i := 0; i < 1000; i++ {
-		select {
-		case <-ninja1:
-			ninja1Count++
-		case <-ninja2:
-			ninja2Count++
-		}
-	}
-	fmt.Printf("ninja1 %d vs %d ninja2\n", ninja1Count, ninja2Count)
+func attack(attacker string, evilNinja string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("%s is attacking: %s\n", attacker, evilNinja)
 }
